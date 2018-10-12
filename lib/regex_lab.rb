7c7b4c7179d0d1c00c19c5d40ca737db9a0a0c3c
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-12 02:00:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  return !!word.match(/^[[aeiou]||[AEIOU]]\w*/)
+  return !!word.match(/^[aeiouAEIOU]\w*/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  return !!word.match(//)
+  return !!word.match(/^[unUN]/)
 end
 
 def words_five_letters_long(text)

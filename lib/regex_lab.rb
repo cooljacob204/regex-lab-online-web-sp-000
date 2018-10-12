34134d6910da3f62bd956521b1c182b9e01861e3
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-12 02:08:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  return word.scan(/un\w*ing/)
+  return text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
